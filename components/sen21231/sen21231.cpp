8ee819e99e5f5ba0f1bb6fa8ccd21d472da204c2
Shreyas Karnik <karnik.shreyas@gmail.com>
2022-11-22 21:17:17 -0800

    init

--- components/sen21231/sen21231.cpp
+++ components/sen21231/sen21231.cpp
@@ -6,6 +6,16 @@ namespace sen21231_sensor {
 
 static const char *TAG = "sen21231_sensor.sensor";
 
+void Sen21231Sensor::read_data_() {
+    // read data and cast it into the person_sensor_results_t struct
+    person_sensor_results_t results;
+    this->read_bytes(PERSON_SENSOR_REG_RESULTS, (uint8_t *)&results,
+                     sizeof(results));
+    if (results.num_faces > 0) {
+        ESP_LOGD(TAG, "SEN21231: %d faces detected", results.num_faces);
+        this->publish_state(true);
+    }
+}
 void Sen21231Sensor::setup() {}
 
 void Sen21231Sensor::update() {
@@ -19,17 +29,6 @@ void Sen21231Sensor::dump_config() {
         ESP_LOGE(TAG, "Communication with SEN21231 failed!");
     }
     ESP_LOGI(TAG, "SEN21231: %s", this->is_failed() ? "FAILED" : "OK");
-}
-
-void Sen21231ASensor::read_data_() {
-    // read data and cast it into the person_sensor_results_t struct
-    person_sensor_results_t results;
-    this->read_bytes(PERSON_SENSOR_REG_RESULTS, (uint8_t *)&results,
-                     sizeof(results));
-    if (results.num_faces > 0) {
-        ESP_LOGD(TAG, "SEN21231: %d faces detected", results.num_faces);
-        this->publish_state(true);
-    }
 }
 
 } // namespace sen21231_sensor
