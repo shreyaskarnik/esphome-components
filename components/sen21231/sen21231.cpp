fcd8f88609c181dd7bb1359ea3efc91808be1e70
Shreyas Karnik <karnik.shreyas@gmail.com>
2022-11-22 19:32:35 -0800

    init

--- components/sen21231/sen21231.cpp
+++ components/sen21231/sen21231.cpp
@@ -10,7 +10,14 @@ void Sen21231Sensor::setup() {}
 
 void Sen21231Sensor::update() {}
 
-void Sen21231Sensor::dump_config() {}
+void Sen21231Sensor::dump_config() {
+    ESP_LOGCONFIG(TAG, "SEN21231:");
+    LOG_I2C_DEVICE(this);
+    if (this->is_failed()) {
+        ESP_LOGE(TAG, "Communication with SEN21231 failed!");
+    }
+    LOG_SENSOR("  ", "Face Detected");
+}
 
 } // namespace sen21231_sensor
 } // namespace esphome
